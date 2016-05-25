807c527eb91335755119d7e9e0f2589e97a510bc
wangyan186 <1462846110@qq.com>
2016-05-25 12:14:36 +0800

    Create Guitar.java

---
+++ Guitar.java
@@ -0,0 +1,39 @@
+package beans;
+
+public class Guitar {
+		private int id;
+		private String builder;
+		private String model;
+		private String type;
+		private String wood;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getBuilder() {
+			return builder;
+		}
+		public void setBuilder(String builder) {
+			this.builder = builder;
+		}
+		public String getModel() {
+			return model;
+		}
+		public void setModel(String model) {
+			this.model = model;
+		}
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
+		public String getWood() {
+			return wood;
+		}
+		public void setWood(String wood) {
+			this.wood = wood;
+		}
+}
