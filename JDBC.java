e8be4e53278ed14c16f555b25f69a849c7301583
wangyan186 <1462846110@qq.com>
2016-05-25 12:16:30 +0800

    Create JDBC.java

---
+++ JDBC.java
@@ -0,0 +1,42 @@
+package jdbc;
+
+import java.sql.*;
+
+public class JDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JDBC t=new JDBC();
+		System.out.print(t.getConnection());
+	}
+
+	
+	
+	public Connection getConnection(){
+		
+	    try {
+			Class.forName("org.sqlite.JDBC");
+		
+	    Connection conn =
+	      DriverManager.getConnection("jdbc:sqlite://d:/guitars.db");
+	      
+	    	return conn;
+	    } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return null;
+		
+	}
+	public void closeConnection(Connection conn){
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
